7e30dba198ee653001fac67115a41bf79ec001c7
mygit-sampath-govardhan <18311A0425@sreenidhi.edu.in>
2023-08-07 10:56:58 +0530

    demo

---
+++ adder_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module adder_tb( );
+reg a;
+reg b;
+wire s;
+wire c;
+adder1 uut(.a(a),.b(b),.s(s),.c(c));
+initial begin
+#10
+a=0;
+b=0;
+#10
+a=0;
+b=1;
+#10
+a=1;
+b=0;
+#10
+a=1;
+b=1;
+#10
+$stop;
+end
+always @(a or b) begin
+    $display("%b + %b = %b with carry %b at time %t", a, b, s, c, $time);
+end
+endmodule
---
+++ hello.tcl
@@ -0,0 +1,59 @@
+#!/usr/bin/tclsh
+
+
+puts "HALF ADDER WITHOUT GUI"
+
+set project_name "halfadder_tcl"
+
+set source_file "adder1.v" 
+set testbench_file "adder_tb.v"
+
+create_project $project_name ./using_tcl/$project_name -part xc7z020clg484-1
+
+add_files -fileset sources_1 $source_file
+add_files -fileset sim_1 $testbench_file
+
+
+set_property top adder_tb [current_fileset]
+
+current_project $project_name
+
+set_property target_simulator "XSim" [current_project]
+
+set_property top adder_tb [get_filesets sim_1]
+
+launch_simulation 
+create_wave_config waveform
+save_wave_config waveform
+close_wave_config waveform
+close_sim
+
+set_property top adder1 [current_fileset]
+
+synth_design -top adder1
+
+#set_property target_simulator "XSim" [current_project]
+#launch_simulation -mode "post-synthesis" -type "functional"
+
+set_property IOSTANDARD LVCMOS33 [get_ports a]
+set_property IOSTANDARD LVCMOS33 [get_ports b]
+set_property IOSTANDARD LVCMOS33 [get_ports c]
+set_property IOSTANDARD LVCMOS33 [get_ports s]
+set_property PACKAGE_PIN U7 [get_ports a]
+set_property PACKAGE_PIN W5 [get_ports b]
+set_property PACKAGE_PIN W6 [get_ports c]
+set_property PACKAGE_PIN W7 [get_ports s]
+
+#or read_xdc $filename.xdc
+
+opt_design
+place_design
+route_design
+
+#report_timing_summary
+write_bitstream -force ./using_tcl/output.bit
+
+close_project
+
+exit
+
